42bea073f402f6f8b1783edbae23f168d63d4e90
Edward <eddieim@gmail.com>
2020-11-30 13:01:07 -0800

    W1D1 afternoon

---
+++ first_java_program/Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    // POINT OF ENTRY METHOD
+    public static void main(String args[]){
+        System.out.println("My name is Edward");
+        System.out.println("I am 30");
+        System.out.println("I'm from Los Angeles");
+    }
+}
