d47aa818f4a50f4763fd91fe1668ca9ef6573332
Edward <eddieim@gmail.com>
2020-11-30 09:29:29 -0800

    W1D1 morning

--- algos/week_1/algo.js
+++ algos/week_1/algo.js
@@ -1,6 +1,59 @@
 // MON
-
-
+class SLNode{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class SLL{
+    constructor(){
+        this.head = null
+    }
+    insertAtFront(value){
+        // CHECK TO SEE IF LIST IS EMPTY
+        // IF EMPTY, CREATE NODE AND SET HEAD TO NEW NODE
+        if(this.head == null){
+            var newNode = new SLNode(value)
+            this.head = newNode
+        }
+        // IF LIST IS NOT EMPTY
+        else{
+            // CREATE THE NEW NODE
+            var newNode = new SLNode(value)
+            // POINT NEW NODE'S .NEXT TO THE HEAD
+            newNode.next = this.head
+            // CHANGE THE HEAD POINTER TO NEW NODE
+            this.head = newNode
+        }
+    }
+    removeAtFront(){
+        // MOVE HEAD POINTER TO SECOND NODE
+        if(this.head == null){
+            console.log("LIST IS EMPTY")
+        }
+        // LIST IS NOT EMPTY
+        else{
+            var value = this.head.value
+            this.head = this.head.next
+            return value
+        }
+    }
+    printValues(){
+        var runner = this.head
+        while(runner.next != null){
+            console.log(runner.value)
+            runner = runner.next
+        }
+        console.log(runner.value)
+    }
+}
+var list = new SLL()
+list.insertAtFront(1)
+list.insertAtFront(2)
+list.insertAtFront(3)
+list.printValues()
+list.removeAtFront()
+list.printValues()
 
 // -----------------------------------------------------------------------------------------------//
 // -----------------------------------------------------------------------------------------------//
---
+++ algos/week_1/group_5.js
@@ -0,0 +1,12 @@
+class Node{
+    constructor(value){
+        this.value = val
+        this.next = null
+    }
+}
+
+class SLL{
+    constructor(){
+        this.head = null
+    }
+}
