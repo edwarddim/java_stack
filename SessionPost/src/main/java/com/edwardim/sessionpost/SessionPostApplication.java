199f3b87012f6f2d641e3bcdf1bf2ac01f4985c2
Edward <eddieim@gmail.com>
2021-04-13 12:21:08 -0600

    W2D2 morning

---
+++ SessionPost/src/main/java/com/edwardim/sessionpost/SessionPostApplication.java
@@ -0,0 +1,13 @@
+package com.edwardim.sessionpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionPostApplication.class, args);
+	}
+
+}
