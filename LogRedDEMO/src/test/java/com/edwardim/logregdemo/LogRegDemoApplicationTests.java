346dcdeffdc5be64d51c5916295e7ddd4aeec4b7
Edward <eddieim@gmail.com>
2021-04-19 12:23:07 -0600

    W3D1 morning

---
+++ LogRedDEMO/src/test/java/com/edwardim/logregdemo/LogRegDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardim.logregdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRegDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
