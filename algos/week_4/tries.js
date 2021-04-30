8d5f0c736526d2dc7dd25cf62a4443f1fcf6b7b7
sadieBoBadie <sflick@codingdojo.com>
2021-04-30 10:38:22 -0700

    Trie AutoComplete  Soln

--- algos/week_4/tries.js
+++ algos/week_4/tries.js
@@ -29,13 +29,49 @@ class Trie {
         return this;
     }
 
-    // RECURSIVE
-    addRecursive(word, node = this.root, partial = "") {
-        // when val str has been sliced all the way it will be empty str
-        // NAH.
+    /**
+     * Returns an array with any complete words beginning
+     * with the given starting substring.
+     * - Time: O(?)
+     * - Space: O(?)
+     * @returns {array} of strings
+     */
+    autoComplete(startingSubstring) {
+        let startNode = this.getLastOfSubstring(startingSubstring);
+        return this.getCompletions(startingSubstring, "", startNode);
+    }
+
+    // Finds the last letter node of the given substring
+    getLastOfSubstring(substring) {
+        let runner = this.root;
+        for (let letter of substring) {
+            if (runner.children.hasOwnProperty(letter)) {
+                runner = runner.children[letter];
+            }
+        }
+        // console.log("last node letter: ", runner.letter);
+        return runner;
+    }
+
+    // RECURSIVE CALL TO GET WORD COMBINATIONS
+    getCompletions(prepend="", branchString="", node=this.root, completions=[]) {
+        if (node.endWord) {
+            completions.push(prepend+branchString);
+        }
+        if (!node.children) {
+            return completions; 
+        }
+        for (let letter in node.children) {
+            this.getCompletions(prepend, branchString + node.children[letter].letter, node.children[letter], completions);
+        }
+        // console.log("end? ", completions);
+        return completions;
     }
 }
 
 let searchHistory = new Trie();
 searchHistory.add("cat").add("can").add("candy").add("apple").add("a").add("an").add("and");
 console.log(searchHistory);
+
+console.log(searchHistory.autoComplete("a"));
+console.log(searchHistory.autoComplete("ca"));
