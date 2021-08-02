1d07dd0c82d6b96072edc7d7d397cbcc8fd69081
Edward <eddieim@gmail.com>
2021-08-02 11:08:50 -0700

    W1D1 morning

--- algos/week_1/algo.js
+++ algos/week_1/algo.js
@@ -9,33 +9,33 @@ Intro to Singly Linked Lists
 
 
 // Part I
-// let a = new SLNode(7);
-// let b = new SLNode(3);
-// let c = new SLNode(1);
-// a.next = b;
-// b.next = c;
+let a = new SLNode(7);
+let b = new SLNode(3);
+let c = new SLNode(1);
+a.next = b;
+b.next = c;
 
-// console.log('A:', a.val);
-// console.log('B:', b.val);
-// console.log('C:', c.val);
-// console.log(a.next.val);
-// console.log(a.next.next.val);
-// console.log(a.next.next.next.val);
+console.log('A:', a.val);
+console.log('B:', b.val);
+console.log('C:', c.val);
+console.log(a.next.val);
+console.log(a.next.next.val);
+console.log(a.next.next.next.val);
 
 // Part II 
 // Note: Later the head variable shown here will be part of the SLL class
 
-// let head = new SLNode(1);
-// let runner_temp = head;
-// for (let i of [7,3,5,2]) {
-//     let new_node = new SLNode(i);
-//     runner_temp.next = new_node;
-//     runner_temp = new_node;
-// }
+let head = new SLNode(1);
+let runner_temp = head;
+for (let i of [7,3,5,2]) {
+    let new_node = new SLNode(i);
+    runner_temp.next = new_node;
+    runner_temp = new_node;
+}
 
-// console.log(head.val);
-// console.log(head.next.val);
-// console.log(head.next.next.next.val);
+console.log(head.val);
+console.log(head.next.val);
+console.log(head.next.next.next.val);
 
 
 class SLNode{
