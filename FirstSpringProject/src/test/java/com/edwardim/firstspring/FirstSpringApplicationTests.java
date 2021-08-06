e34a4c628fd0dd84d7760c40c08a6aa11e413403
Edward <eddieim@gmail.com>
2021-08-06 16:49:00 -0700

    added first spring

---
+++ FirstSpringProject/src/test/java/com/edwardim/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardim.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
