cf5891cd372974aca896f998123d6baf06c18ae0
Edward <eddieim@gmail.com>
2021-08-09 11:04:36 -0700

    W2D1 morning

---
+++ SessionPOST/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<meta charset="ISO-8859-1">
+	<title>Insert title here</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Hello World</h1>
+		<form method="POST" action="/process">
+			<input type="email" name="email" />
+			<input type="password" name="password" />
+			<button type="submit">Log In</button>
+		</form>
+	</div>
+</body>
+</html>
---
+++ algos/week_2/algo.js
@@ -0,0 +1,94 @@
+// MON
+// LAST IN FIRST OUT
+// LIFO - STACKS
+class Stack {
+    constructor(items = []) {
+        this.items = items;
+    }
+    // Time: O(1)
+    // Space: O(1)
+    push(item) {
+        this.items.push(item)
+    }
+
+    // Time: O(1)
+    // Space: O(1)
+    // Returns undefined if empty
+    pop() {
+        return this.items.pop()
+    }
+
+    // aka top, returns undefined if empty
+    // Time: O(1)
+    // Space: O(1)
+    peek() {
+        return this.items[this.items.length - 1]
+    }
+
+    // Time: O(1)
+    // Space: O(1)
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    // Time: O(1)
+    // Space: O(1)
+    size() {
+
+    }
+
+    // Time: O(n) linear
+    // Space: O(n)
+    print() {
+
+    }
+}
+
+class SLNode {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class SLStack {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    isEmpty() {
+        
+    }
+
+    // Adds a new node with the given value in front of the head node.
+    push(value) {
+
+    }
+
+    // Removes the head node
+    // return the popped value -- important!
+    pop() {
+
+    }
+
+    size() {
+
+    }
+
+    peek() {
+
+    }
+    printPretty() {
+
+    }
+
+    ////  EXTRA  /////
+    // Reverses stack with the order of the nodes reversed
+    // For this exercise, do NOT try to re-point all the pointers
+    // use an extra stack as storage and utilize the pop and push 
+    // methods. Hint: you can re-point the head.
+    reverse() {
+
+    }
+}
