40c565a036bec3145b6437b28bc8f779d661884a
Edward Im <eddieim@gmail.com>
2022-04-25 14:18:23 -0700

    W4D1 afternoon

--- algos/week_4/stacks.js
+++ algos/week_4/stacks.js
@@ -3,7 +3,7 @@
  * Follows a LIFO (Last In First Out) order where new items are stacked on
  * top (back of array) and removed items are removed from the top / back.
  */
- class Stack {
+class Stack {
     /**
      * The constructor is executed when instantiating a new Stack() to construct
      * a new instance.
@@ -21,7 +21,9 @@
      * @param {any} item The new item to be added to the top / back.
      * @returns {number} The new length of this stack.
      */
-    push(item) {}
+    push(item) {
+      this.items.push(item)
+    }
   
     /**
      * Removes the top / last item from this stack.
@@ -29,7 +31,9 @@
      * - Space: O(1) constant.
      * @returns {any} The removed item or undefined if this stack was empty.
      */
-    pop() {}
+    pop() {
+      return this.items.pop()
+    }
   
     /**
      * Retrieves the top / last item from this stack without removing it.
@@ -37,7 +41,9 @@
      * - Space: O(1) constant.
      * @returns {any} The top / last item of this stack.
      */
-    peek() {}
+    peek() {
+      return this.items[this.items.length - 1]
+    }
   
     /**
      * Returns whether or not this stack is empty.
@@ -45,7 +51,9 @@
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
+    isEmpty() {
+      return this.items.length === 0
+    }
   
     /**
      * Returns the size of this stack.
@@ -53,5 +61,73 @@
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
+    size() {
+      return this.size.length
+    }
+}
+
+class StackNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
+}
+
+class LinkedListStack {
+  constructor() {
+    this.head = null;
+  }
+
+    /**
+     * Adds a new given item to the top / back of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to be added to the top / back.
+     * @returns {number} The new length of this stack.
+     */
+    push(item) {
+
+    }
+  
+    /**
+     * Removes the top / last item from this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item or undefined if this stack was empty.
+     */
+    pop() {
+
+    }
+  
+    /**
+     * Retrieves the top / last item from this stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The top / last item of this stack.
+     */
+    peek() {
+
+    }
+  
+    /**
+     * Returns whether or not this stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+
+    }
+  
+    /**
+     * Returns the size of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() {
+
+    }
+
+
+}
