0a049862083450d6375d1eca04ec1bbd2908a0d1
Edward <eddieim@gmail.com>
2022-06-05 12:06:24 -0700

    start of June 2022

--- inheritancedemo/GravityInterface.java
+++
@@ -1,8 +0,0 @@
-package com.edwardim.inheritancedemo;
-
-public interface GravityInterface {
-	// STATIC AND FINAL VARIABLES
-	double gravity = 9.8;
-	
-	void fly();
-}
--- inheritancedemo/SwimmingInterface.java
+++
@@ -1,7 +0,0 @@
-package com.edwardim.inheritancedemo;
-
-public interface SwimmingInterface {
-	double waterViscocity = 1.4;
-	
-	void swimming();
-}
