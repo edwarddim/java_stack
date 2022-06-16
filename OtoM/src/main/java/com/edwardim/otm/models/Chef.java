a398629b368d739e98385ffbc272166ea31398d6
Edward Im <eddieim@gmail.com>
2022-06-16 16:07:50 -0700

    W2D4 afternoon

---
+++ OtoM/src/main/java/com/edwardim/otm/models/Chef.java
@@ -0,0 +1,116 @@
+package com.edwardim.otm.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "chefs")
+public class Chef {
+	// MEMBER VARIABLES
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    private String name;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date dob;
+    
+    // RELATIONSHIPS
+    @OneToMany(mappedBy="creator", fetch = FetchType.LAZY)
+    private List<Recipe> recipes;
+    
+    
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    // CONSTRUCTORS
+    public Chef() {
+    	
+    }
+    
+	public Chef(String name, Date dob) {
+		super();
+		this.name = name;
+		this.dob = dob;
+	}
+
+	// GETTERS / SETTERS / OTHER METHODS
+
+
+
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+	
+	@PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public List<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(List<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
