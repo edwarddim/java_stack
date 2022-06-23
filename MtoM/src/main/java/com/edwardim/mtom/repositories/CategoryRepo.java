fbf7e61e4589f0affc60d7baf9b8f86b625625b4
Edward Im <eddieim@gmail.com>
2022-06-23 13:31:27 -0700

    W3D4 morning

---
+++ MtoM/src/main/java/com/edwardim/mtom/repositories/CategoryRepo.java
@@ -0,0 +1,13 @@
+package com.edwardim.mtom.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edwardim.mtom.models.Category;
+
+@Repository
+public interface CategoryRepo extends CrudRepository<Category, Long> {
+	List<Category> findAll();
+}
