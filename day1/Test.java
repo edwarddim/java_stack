1372e670b9d32efdc48204503eef3867a14003d5
Edward Im <eddieim@gmail.com>
2022-11-17 14:37:56 -0800

    Dec reset

--- day1/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test{
-    // POINT OF ENTRY METHOD
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        System.out.println("Hello again");
-    }
-}
